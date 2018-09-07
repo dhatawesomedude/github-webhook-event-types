d9e6e64475ef3e48934e0808d8f88786f01169ec
james <jmosul@gmail.com>
2018-09-07 21:58:54 +0100

    refactor: give common types to extend

--- source/InstallationRepositories.d.ts
+++ source/InstallationRepositories.d.ts
@@ -1,29 +1,10 @@
-export interface InstallationRepositoriesAccount {
-  login: string;
-  id: number;
-  avatar_url: string;
-  gravatar_id: string;
-  url: string;
-  html_url: string;
-  followers_url: string;
-  following_url: string;
-  gists_url: string;
-  starred_url: string;
-  subscriptions_url: string;
-  organizations_url: string;
-  repos_url: string;
-  events_url: string;
-  received_events_url: string;
-  type: string;
-  site_admin: boolean;
+import {Action, Installation, User} from './common';
+
+export interface InstallationRepositoriesAccount extends User {
 }
 
-export interface InstallationRepositoriesInstallation {
-  id: number;
+export interface InstallationRepositoriesInstallation extends Installation {
   account: InstallationRepositoriesAccount;
-  repository_selection: string;
-  access_tokens_url: string;
-  repositories_url: string;
   html_url: string;
 }
 
@@ -33,28 +14,10 @@ export interface InstallationRepositoriesRepositories_removed {
   full_name: string;
 }
 
-export interface InstallationRepositoriesSender {
-  login: string;
-  id: number;
-  avatar_url: string;
-  gravatar_id: string;
-  url: string;
-  html_url: string;
-  followers_url: string;
-  following_url: string;
-  gists_url: string;
-  starred_url: string;
-  subscriptions_url: string;
-  organizations_url: string;
-  repos_url: string;
-  events_url: string;
-  received_events_url: string;
-  type: string;
-  site_admin: boolean;
+export interface InstallationRepositoriesSender extends User {
 }
 
-export interface InstallationRepositories {
-  action: string;
+export interface InstallationRepositories extends Action {
   installation: InstallationRepositoriesInstallation;
   repository_selection: string;
   repositories_added: any[];
--- source/Team.d.ts
+++ source/Team.d.ts
@@ -1,52 +1,25 @@
+import {Action, Organization, User} from './common';
+
 export interface TeamTeam {
-  name: string;
-  id: number;
-  slug: string;
-  description: string;
-  privacy: string;
-  url: string;
-  members_url: string;
-  repositories_url: string;
-  permission: string;
+    name: string;
+    id: number;
+    slug: string;
+    description: string;
+    privacy: string;
+    url: string;
+    members_url: string;
+    repositories_url: string;
+    permission: string;
 }
 
-export interface TeamOrganization {
-  login: string;
-  id: number;
-  url: string;
-  repos_url: string;
-  events_url: string;
-  hooks_url: string;
-  issues_url: string;
-  members_url: string;
-  public_members_url: string;
-  avatar_url: string;
-  description: string;
+export interface TeamOrganization extends Organization {
 }
 
-export interface TeamSender {
-  login: string;
-  id: number;
-  avatar_url: string;
-  gravatar_id: string;
-  url: string;
-  html_url: string;
-  followers_url: string;
-  following_url: string;
-  gists_url: string;
-  starred_url: string;
-  subscriptions_url: string;
-  organizations_url: string;
-  repos_url: string;
-  events_url: string;
-  received_events_url: string;
-  type: string;
-  site_admin: boolean;
+export interface TeamSender extends User {
 }
 
-export interface Team {
-  action: string;
-  team: TeamTeam;
-  organization: TeamOrganization;
-  sender: TeamSender;
+export interface Team extends Action {
+    team: TeamTeam;
+    organization: TeamOrganization;
+    sender: TeamSender;
 }
